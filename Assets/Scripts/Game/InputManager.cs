f4fc7ef35916efb2d73d7bbe21e885792f3c4212
dmurtagh <damien.murtagh@gmail.com>
2018-07-07 15:39:40 -0700

    InputManager, Joypad support, double jump

---
+++ Assets/Scripts/Game/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+    public float m_ControllerDeadzone = 0.11f;
+
+    public bool GetJumpKeyDown()
+    {
+        return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0);
+    }
+
+    public bool GetResetKeyDown()
+    {
+        return Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button6);
+    }
+
+    public float GetLeftStickHorizontal()
+    {
+        float keyboardVal = Input.GetAxis("Horizontal");
+        float gamepadVal = Input.GetAxis("joy_0_axis_0");
+
+        if (Mathf.Abs(keyboardVal) > Mathf.Abs(gamepadVal))
+        {
+            return keyboardVal;
+        }
+        else
+        {
+            if (Mathf.Abs(gamepadVal) < m_ControllerDeadzone)
+            {
+                return 0f;
+            }
+            return gamepadVal;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_MaxSpeed = 10f;
+    public float m_JumpForce = 700f;
+
+    // For movement
+    Rigidbody2D m_Rigidbody2d;
+    bool m_FacingRight = true;
+
+    // For animation
+    Animator m_Animator;
+
+    // For ground collisions
+    public LayerMask m_WhatIsGround;
+    public Transform m_GroundCheck;
+    bool m_Grounded = false;
+    float m_GroundRadius = 0.2f;
+
+    // For jumping
+    bool m_DoubleJump = false;
+
+    // Debug stuff
+    Vector2 m_StartPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_Rigidbody2d = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+        m_Animator = GetComponent(typeof(Animator)) as Animator;
+        m_StartPosition = m_Rigidbody2d.position;
+	}
+
+    private void Update()
+    {
+        CheckForJump();
+        CheckForReset();
+    }
+
+    private void CheckForReset()
+    {
+        if (InputManager.Instance.GetResetKeyDown())
+        {
+            m_Rigidbody2d.position = m_StartPosition;
+        }
+    }
+
+    private void CheckForJump()
+    {
+        if ((m_Grounded || !m_DoubleJump) && 
+            InputManager.Instance.GetJumpKeyDown())
+        {
+            if(!m_Grounded)
+            {
+                m_Rigidbody2d.velocity = new Vector2(m_Rigidbody2d.velocity.x, 0f); // Zero out the y velocity so the jump has equal effect, whether we're stationary, jumping or falling
+            }
+
+            m_Animator.SetBool("Ground", false);
+            m_Rigidbody2d.AddForce(new Vector2(0, m_JumpForce));
+
+            if (!m_DoubleJump && !m_Grounded)
+            {
+                m_DoubleJump = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        m_Grounded = Physics2D.OverlapCircle(m_GroundCheck.position, m_GroundRadius, m_WhatIsGround);
+        m_Animator.SetBool("Ground", m_Grounded);
+
+        if (m_Grounded)
+        {
+            m_DoubleJump = false;
+        }
+
+        m_Animator.SetFloat("vSpeed", m_Rigidbody2d.velocity.y);
+
+        float move = InputManager.Instance.GetLeftStickHorizontal();
+
+        m_Animator.SetFloat("Speed", Mathf.Abs(move));
+        m_Rigidbody2d.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2d.velocity.y);
+
+        if (move > 0 && !m_FacingRight)
+        {
+            flip();
+        }
+        else if (move < 0 && m_FacingRight)
+        {
+            flip();
+        }
+	}
+
+    void flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
